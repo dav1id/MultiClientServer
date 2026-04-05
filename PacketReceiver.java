d006fdf8443f1ef13790658cd5892468f209975d
David Ola <doola@ualberta.ca>
2026-04-05 16:20:49 -0600

    made sync

--- PacketReceiver.java
+++ PacketReceiver.java
@@ -2,56 +2,58 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-import java.net.Socket;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class PacketReceiver implements Runnable {
     private PacketTransceiver packetTransceiver = null;
-    private Socket socket;
-    private String message;
-
+    private final Socket socket;
     private boolean status;
 
+    private Queue<String> messageQueue;
 
     public PacketReceiver(PacketTransceiver packetTransceiver, Socket socket){
         this.packetTransceiver = packetTransceiver;
         this.socket = socket;
-    }
 
-    public void setStatus(){
-        status = false;
-    }
-
-
-    /*
-        I can pass the lock in PacketReceiver as well. Once the client receives the response from its wait() (client
-        is closed), the client is going to send a notify to run here.
-
-       I can create a second thread inside of the PacketReceiver that incrementally checks a boolean value assigned to
-       PacketTransceiver. If PacketTransceiver sees the
-
-       Pass a reference to PacketReceiver from PacketTransceiver
-    */
-    public void run(){
         status = true;
+
+        messageQueue = new ArrayDeque<>();
+    }
+
+    public void run(){
         try(BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+            Thread inputFilter = new Thread(() -> {
+
+                String message;
+                while (status){
+                    if ((message = messageQueue.peek()) != null){
+                        String[] concat = message.split("-", 1);
+
+                        if (concat[0].equals("server")){
+                            if ((packetTransceiver.messageFunc(message, "setServerMessage"))){
+                                System.out.println(message);
+                            }
+                        }
+                    }
+                }
+            });
+            inputFilter.start();
+
             while (status){
                 String message;
                 if ((message = in.readLine()) != null){
-                    String[] concat = message.split("-", 1);
-
-                    if (concat[0].equals("server")){
-                        if (!(packetTransceiver.setServerMessage(message))){
-                           System.out.println("Invalid server message...."); //Find out what to do here, don't want to create an outputstream here. Might need to create a PacketTransceiver method to send messages.
-                        }
-                    } else{
-                        System.out.println(message);
-                    }
+                    messageQueue.add(message);
                 }
             }
         } catch (IOException e){
             Thread.currentThread().interrupt();
             System.out.println("Packet Receiver Error: Client cannot sucessfully receive input from the server.");
         }
+    }
+
+    public void setStatus(Boolean status){
+        status = false;
     }
 }
--- PacketTransceiver.java
+++ PacketTransceiver.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 import java.util.ArrayList;
@@ -16,6 +20,8 @@ public class PacketTransceiver implements Runnable {
     private boolean status = false;
     private final Object lock;
 
+    private Queue<String> messageQueue;
+
     /**
      Initialises the PacketTransceiver that will act as a thread once run() is called.
      Socket is the client socket that we'll be using to communicate with the server,
@@ -28,6 +34,8 @@ public class PacketTransceiver implements Runnable {
         this.socket = socket;
         this.args = args;
         this.lock = Client.lock;
+
+        messageQueue = new ArrayDeque<>();
     }
 
     /**
@@ -41,16 +49,27 @@ public class PacketTransceiver implements Runnable {
     }
 
     /**
-     Server is going to send a message to PacketReceiver when the number of clients have been changed.
-     The PacketReceiver is going to decode the server message, and have a reference to PacketTransceiver
-     to change the number of clients list that is necessary for the PacketTransceiver to encode its message.
-
-     Might want to look into making it into a switch statement
-
-     Server is going to send a message with remove followed by the clients to remove. And add, followed by the clients to add.
-     Using the format -> "server-UpdateClients-remove client1 client2 client 3 - add client 1 client 2 client 3"
+        Allows only one thread to call setServerMessage or checkMessage at a time. In case one thread is trying to
+        change the list of clients, while another one is using old info to check if the message can be sent. Ideally
+        the thread sending a server message should have more priority -> Limitation of my design that I've been trying
+        to find a solution to. The server just won't send the message if the client does not exist instead of making an
+        exception.
      **/
+    public synchronized boolean messageFunc(String message, String command){
+        if (command.equals("setServerMessage")){
+            return setServerMessage(message);
+        } else{
+            return checkMessage(message);
+        }
+    }
 
+    /**
+        PacketReceiver forwards the message from the server. i.e., a message like:
+            "server -UpdateClients -remove client1 client2 client 3 -add client1 client2 client3"
+
+        Only message that the server can send so far is a notification to all clients to change the number of clients
+        when a client leaves or joins.
+     **/
     public boolean setServerMessage(String serverMessage) {
         String[] concat = serverMessage.split("-");
 
@@ -65,15 +84,16 @@ public class PacketTransceiver implements Runnable {
                 for (String add : addList)
                     clientsList.add((String) add);
 
-                return true; // Client understands message and responds to PacketReceiver by sending true
+                return true;
+
+            default:
+                return false;
         }
-        return false;
     }
 
     /**
      Checks if the message aligns with the format. The message should contain a client that exists in the clientsList
      **/
-
     public boolean checkMessage(String message){
         String[] messageSeperator = message.split(" ", 1);
 
@@ -81,6 +101,19 @@ public class PacketTransceiver implements Runnable {
 
         if (correctMessage) return true;
         return false;
+    }
+
+
+    public synchronized void queueFunctions(String message, String function){
+        switch(function){
+            case "add":
+                messageQueue.add(message);
+            case "remove":
+                messageQueue.remove();
+                break;
+            default:
+                break;
+        }
     }
 
     /**
@@ -91,38 +124,41 @@ public class PacketTransceiver implements Runnable {
 
      2. Input in terms of a server message.
      **/
-
     public void run(){
         try(
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 Scanner scanner = new Scanner(System.in)
         ){
 
-            ArrayList<String> messageList = new ArrayList<>();
-
             Thread inputThread = new Thread(() -> {
                 String message;
 
                 while (!(message = scanner.nextLine()).equals("close")) {
                     synchronized(this) {
-                        messageList.add(message);
+                        if (checkMessage(message)) { // To make sure tha
+                            queueFunctions(message, "add");
+                        } else {
+                            System.out.println("Message cannot be sent to someone who is offline");
+                        }
                     }
                 }
+
+                packetReceiver.setStatus(false);
             });
             inputThread.start();
 
-            while (true){ //Look into finding a way to pause this as changeByServer is running
-                if (!(messageList.isEmpty())){
-                    synchronized(this) {
-                        String message = messageList.getFirst();
-                        if (message.equals("close")) break;
+            while (true){
+                String message;
+                if ((message = messageQueue.peek()) != null){
+                    if (message.equals("close")) break;
 
-                        if (checkMessage(message)) {
-                            out.println(message);
-                            messageList.removeFirst();
-                        } else {
-                            System.out.println("This message cannot be sent because this client does not exist"); // Will be handled by back-end once the GUI is running
+                    if (checkMessage(message)){
+                        out.println(message);
+                        synchronized(this) {
+                            queueFunctions(message, "remove");
                         }
+                    } else {
+                        System.out.println("This message cannot be sent because this client does not exist"); // Will be handled by back-end once the GUI is running
                     }
                 }
             }
