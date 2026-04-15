f4d5e3fd054ab2ac142d0e105a86bc46137ccdca
David Ola <doola@ualberta.ca>
2026-04-14 23:38:26 -0600

    created consumerTask

--- v2/Server.java
+++ v2/Server.java
@@ -8,30 +8,70 @@ package v2;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+
 import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server {
+public class Server implements Runnable {
     private boolean status;
     public int clientCounter;
 
-    public boolean consumerTask() {
-        return true;
+    public boolean consumerTask(Set<SelectionKey>  selectionKeys, SelectionKey sender, ExecutorService consumerThreadPool) {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        try(SocketChannel client = (SocketChannel) sender.channel()){
+            int result;
+            do {
+                result = client.read(buffer);
+
+                if (result == 0){
+                    throw new RuntimeException(); // Don't know what to do here
+                }
+            } while(result != -1);
+
+            String message = new String(buffer.array(), 0, buffer.limit());
+            String[] messageArray = message.split(" ");
+
+            for(SelectionKey receiver : selectionKeys){
+                ClientMeta clientMeta = (ClientMeta) receiver.attachment();
+
+                if (clientMeta.getClientName().equals(messageArray[0])){
+
+                    consumerThreadPool.submit(
+                            () -> {
+                                producerTaskWithoutIteration(sender, receiver);
+                            }
+                    );
+
+                    return true;
+                }
+            }
+
+
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        return false;
     }
 
-    public boolean producerTask(){
-        return true;
+    public void producerTaskWithoutIteration(SelectionKey senderKey, SelectionKey receiverKey){
     }
 
-    public void attachClientMeta(){
 
-    };
+    public boolean producerTask(Set<SelectionKey> selectionKeys, SelectionKey producer){
+        ByteBuffer buffer = ByteBuffer.allocate(1024); // Need to understand ThreadLocals to know where to go next with this
+        return true;
+    }
 
     public void run(){
         status = true;
@@ -58,6 +98,7 @@ public class Server {
 
                     if (selectKey.isAcceptable()){
                         try(SocketChannel client = serverSocketChannel.accept()){
+
                             client.configureBlocking(false);
                             client.register(selector, SelectionKey.OP_READ | SelectionKey.OP_ACCEPT);
                             System.out.println("New client has been connected!");
@@ -66,9 +107,15 @@ public class Server {
                         }
                         // Accept a new socket channel connection here
                     } else if (selectKey.isReadable()){
-                        producerThreadPool.submit( () -> {producerTask();} );
+                        consumerThreadPool.submit( () -> {
+                            consumerTask(selectionKeySet, selectKey, consumerThreadPool);
+                            // Space for some future backlog
+                        });
                     } else if (selectKey.isWritable()){
-                        consumerThreadPool.submit( () -> {consumerTask();} );
+                        producerThreadPool.submit( () -> {
+                            producerTask(selectionKeySet, selectKey);
+                            // Space for some future backlog
+                        });
                     }
                 }
             }
